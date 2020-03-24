2e311775105b15cc534707fed45edd08cffb71fa
wngsdx <wng.sdxw@gmail.com>
2020-03-23 21:53:49 -0700

    4 files modified, 6 added

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,7 +2,7 @@ export const products = [
   {
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
   },
   {
     name: 'Phone Mini',
